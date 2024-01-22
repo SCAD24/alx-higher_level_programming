bfe9aed2ac23aee6ce8946c21b3d12e86412cae9
SCAD24 <scad2468@gmail.com>
2024-01-22 06:21:14 -0800

    Data Structures

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# File: 6-print_matrix_integer.py
+
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers."""
+    for row in matrix:
+        for column in row:
+            print("{:d}".format(column), end=" " if column != row[-1] else "")
+        print()
